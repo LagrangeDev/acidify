ad2a71bdd22f1b5bc8ca4c78f50082f9c54cbb85
Wei FeiYang <wesley.f.young@outlook.com>
2026-02-05 17:58:32 +0800

    feat(yogurt): implement script loader

--- yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/YogurtApp.kt
+++ yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/YogurtApp.kt
@@ -15,14 +15,8 @@ import io.ktor.server.plugins.di.*
 import io.ktor.server.routing.*
 import io.ktor.server.sse.*
 import io.ktor.server.websocket.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.shareIn
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import kotlinx.io.buffered
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
@@ -44,6 +38,8 @@ import org.ntqqrev.yogurt.event.configureMilkyEventAuth
 import org.ntqqrev.yogurt.event.configureMilkyEventSse
 import org.ntqqrev.yogurt.event.configureMilkyEventWebSocket
 import org.ntqqrev.yogurt.event.configureMilkyEventWebhook
+import org.ntqqrev.yogurt.script.Script
+import org.ntqqrev.yogurt.script.loadScripts
 import org.ntqqrev.yogurt.transform.transformAcidifyEvent
 import org.ntqqrev.yogurt.util.*
 
@@ -169,6 +165,25 @@ object YogurtApp {
             }
         }
 
+        if (!SystemFileSystem.exists(scriptsPath)) {
+            SystemFileSystem.createDirectories(scriptsPath)
+        }
+        val scripts = SystemFileSystem.list(scriptsPath)
+            .filter { it.name.endsWith(".yogurtx.js") }
+            .map {
+                async {
+                    Script(
+                        name = it.name,
+                        content = SystemFileSystem.source(it).buffered().use { r ->
+                            withContext(Dispatchers.IO) {
+                                r.readString()
+                            }
+                        }
+                    )
+                }
+            }
+            .awaitAll()
+
         monitor.subscribe(ApplicationStarted) {
             if (config.webhookConfig.url.isNotEmpty()) {
                 configureMilkyEventWebhook()
@@ -177,7 +192,12 @@ object YogurtApp {
             configureSessionStoreAutoSave()
             configureEventLogging()
 
-            launch { bot.login(preloadContacts = config.preloadContacts) }
+            launch {
+                bot.login(preloadContacts = config.preloadContacts)
+                if (scripts.isNotEmpty()) {
+                    loadScripts(scripts)
+                }
+            }
         }
     }
 }
