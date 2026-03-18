fb18fc120cf69abad97f687df2a939d4b1e013b7
Wei FeiYang <wesley.f.young@outlook.com>
2026-03-18 19:43:31 +0800

    feat: add Lagrange sign provider

---
+++ acidify-core-lagrange-sign-provider/src/commonMain/kotlin/org/ntqqrev/acidify/lagrange/LagrangeSignProvider.kt
@@ -0,0 +1,46 @@
+package org.ntqqrev.acidify.lagrange
+
+import org.ntqqrev.acidify.common.SignProvider
+import org.ntqqrev.acidify.common.SignResult
+import org.ntqqrev.acidify.lagrange.sign.Sign
+
+class LagrangeSignProvider(
+    address: String,
+    port: Int,
+    useHttps: Boolean,
+    val uin: Long,
+    val guid: ByteArray,
+    val qua: String,
+) : SignProvider {
+    private val channel = io.github.timortel.kmpgrpc.core.Channel.Builder
+        .forAddress(address, port)
+        .apply {
+            if (!useHttps) {
+                usePlaintext()
+            }
+        }
+        .build()
+    private val signStub = Sign.SignStub(channel)
+
+    override suspend fun sign(
+        cmd: String,
+        seq: Int,
+        src: ByteArray
+    ): SignResult {
+        val response = signStub.getSecSign(
+            Sign.GetSecSignRequest(
+                uin = uin,
+                command = cmd,
+                seq = seq,
+                body = src,
+                guid = guid,
+                qua = qua,
+            )
+        )
+        return SignResult(
+            sign = response.secSign,
+            token = response.secToken,
+            extra = response.secExtra,
+        )
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,7 @@ plugins {
 
 include(
     ":acidify-core",
+    ":acidify-core-lagrange-sign-provider",
     ":acidify-core-runner",
     ":yogurt",
     ":yogurt-jvm",
