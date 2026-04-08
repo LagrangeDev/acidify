fe1dddc87b97e60984d82dc1d1dc12c7f4c72a2b
Wei FeiYang <wesley.f.young@outlook.com>
2026-04-08 10:03:16 +0800

    feat(yogurt): add logging on auth failing paths
    close #60

--- acidify-milky/src/commonMain/kotlin/org/ntqqrev/acidify/milky/api/Auth.kt
+++ acidify-milky/src/commonMain/kotlin/org/ntqqrev/acidify/milky/api/Auth.kt
@@ -9,7 +9,9 @@ import org.ntqqrev.acidify.milky.MilkyContext
 context(ctx: MilkyContext)
 fun Route.apiAuth() = install(createRouteScopedPlugin("ApiAuth") {
     onCall { call ->
+        val logger = ctx.bot.createLogger("ApiAuthPlugin")
         if (call.request.headers["Authorization"] != "Bearer ${ctx.httpAccessToken}") {
+            logger.i { "${call.request.local.remoteAddress} 未携带正确的 access token，拒绝访问" }
             call.respond(HttpStatusCode.Unauthorized)
             return@onCall
         }
--- acidify-milky/src/commonMain/kotlin/org/ntqqrev/acidify/milky/event/Auth.kt
+++ acidify-milky/src/commonMain/kotlin/org/ntqqrev/acidify/milky/event/Auth.kt
@@ -9,10 +9,12 @@ import org.ntqqrev.acidify.milky.MilkyContext
 context(ctx: MilkyContext)
 fun Route.eventAuth() = install(createRouteScopedPlugin("EventAuth") {
     onCall { call ->
+        val logger = ctx.bot.createLogger("ApiAuthPlugin")
         if (
             call.request.headers["Authorization"] != "Bearer ${ctx.httpAccessToken}" &&
             call.request.queryParameters["access_token"] != ctx.httpAccessToken
         ) {
+            logger.i { "${call.request.local.remoteAddress} 未携带正确的 access token，拒绝访问" }
             call.respond(HttpStatusCode.Unauthorized)
             return@onCall
         }
